8a052e124207d9419e0deaa00475b08487475f20
MTChen <gene31105@gmail.com>
2017-11-07 11:58:52 +0800

    'prittify'

--- hw_1.php
+++ hw_1.php
@@ -11,10 +11,9 @@
 
 	// Check connection
 
-	if ($conn->connect_error)
-		{
+	if ($conn->connect_error){
 		die("Connection failed: " . $conn->connect_error);
-		}
+	}
 
 	session_start();
 
@@ -22,22 +21,16 @@
 
 	$_SESSION["content"] = "";
 
-	if (isset($_POST['submit']) && $_POST["text"] != "")
-		{
+	if (isset($_POST['submit']) && $_POST["text"] != ""){
 		$_SESSION["content"] = $_POST['text'];
+	}
 
-		// $query = "INSERT INTO `gb` (`id`, `content`) VALUES (NULL, '" . $_POST['text'] . "')";
-		// $result = $conn->query($query);
-
-		}
-
-	if (isset($_POST['store']) && $_POST["store"] != "")
-		{
+	if (isset($_POST['store']) && $_POST["store"] != ""){
 		$query = "INSERT INTO `gb` (`id`, `content`) VALUES (NULL, '" . $_POST['store'] . "')";
 		$result = $conn->query($query);
-		}
+	}
 
-	?>
+?>
 <html>
 	<head>
 		<meta charset="utf-8">
@@ -53,23 +46,22 @@
 				if ($_SESSION["content"] != "") echo $_SESSION["content"]; ?></textarea>
 			<input type="submit" name="submit" value="<?php
 				if ($_SESSION["content"] != "") echo "edit";
-				  else echo "submit"; ?>"/>
+				else echo "submit"; ?>"/>
 		</form>
 		<br/>
 		<?php
 			$query = "SELECT id, content FROM gb";
 			$results = $conn->query($query);
-			?>
+		?>
 		<table border="1">
 			<?php
-				foreach($results as $result)
-					{
+				foreach($results as $result){
 					echo "<tr><td>";
 					echo $result['content'];
 					echo "</td></tr>";
-					}
+				}
 
-				?>
+			?>
 		</table>
 	</body>
 	<script>
