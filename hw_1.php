113f621f1240ded3161566d3786071872f7db4d2
MTChen <gene31105@gmail.com>
2017-11-08 01:43:57 +0800

    'final_fix_minor_problems'

--- hw_1.php
+++ hw_1.php
@@ -1,72 +1,70 @@
-<!DOCTYPE html>
 <?php
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$dbname = "test";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "test";
 
-	// Create connection
+// Create connection
 
-	$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-	// Check connection
+// Check connection
 
-	if ($conn->connect_error){
-		die("Connection failed: " . $conn->connect_error);
-	}
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
-	session_start();
+session_start();
 
-	// init session
+$_SESSION["content"] = "";
 
-	$_SESSION["content"] = "";
-
-	if (isset($_POST['submit']) && $_POST["text"] != ""){
-		$_SESSION["content"] = $_POST['text'];
-	}
-
-	if (isset($_POST['store']) && $_POST["store"] != ""){
-		$query = "INSERT INTO `gb` (`id`, `content`) VALUES (NULL, '" . $_POST['store'] . "')";
-		$result = $conn->query($query);
-	}
+if (isset($_POST['Submit']) && $_POST['text'] != "") {
+    $_SESSION["content"] = $_POST['text'];
+    $query = "INSERT INTO `gb` (`id`, `content`) VALUES (NULL, '" . $_SESSION['content'] . "')";
+    if ($conn->query($query) === TRUE) {
+        $_SESSION['last_id'] = $conn->insert_id;
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+if (isset($_POST['Revise'])) {
+    $_SESSION["content"] = $_POST['text'];
+    $query = "UPDATE gb SET content='" . $_POST['text'] . "' WHERE id=" . $_SESSION['last_id'];
+    $conn->query($query);
+}
 
 ?>
 <html>
-	<head>
-		<meta charset="utf-8">
-		<title>test</title>
-	</head>
-	<script
-		src="https://code.jquery.com/jquery-2.2.4.js"
-		integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI="
-		crossorigin="anonymous"></script>
-	<body>
-		<form method="post">
-			<textarea name="text"><?php
-				if ($_SESSION["content"] != "") echo $_SESSION["content"]; ?></textarea>
-			<input type="submit" name="submit" value="<?php
-				if ($_SESSION["content"] != "") echo "edit";
-				else echo "submit"; ?>"/>
-		</form>
-		<br/>
-		<?php
-			$query = "SELECT id, content FROM gb";
-			$results = $conn->query($query);
-		?>
-		<table border="1">
-			<?php
-				foreach($results as $result){
-					echo "<tr><td>";
-					echo $result['content'];
-					echo "</td></tr>";
-				}
-
-			?>
-		</table>
-	</body>
-	<script>
-		$(window).unload(function(){
-			$.post("hw_1.php", { store: "<?php echo $_SESSION["content"] ?>" } );
-		});
-	</script>
+<head>
+    <meta charset="utf-8">
+    <title>test</title>
+</head>
+<body>
+<form method="post">
+    <textarea name="text" style="margin: 0px; width: 70%; height: 181px;"><?php echo $_SESSION["content"]; ?></textarea>
+    <?php
+    if ($_SESSION["content"] != "")
+        echo "<input type=\"submit\" name=\"Revise\" value=\"Revise\"/>";
+    else
+        echo "<input type=\"submit\" name=\"Submit\" value=\"Submit\"/>";
+    ?>
+</form>
+<br/>
+<?php
+$query = "SELECT id, content FROM gb";
+$results = $conn->query($query);
+$query2 = "SELECT id FROM gb WHERE content='hello world'";
+$id = $conn->query($query2);
+?>
+<table border="1" width="70%">
+    <?php
+    foreach ($results as $result) {
+        echo "<tr><td>";
+        echo $result['content'];
+        echo "</td></tr>";
+    }
+    $conn->close();
+    ?>
+</table>
+</body>
 </html>
